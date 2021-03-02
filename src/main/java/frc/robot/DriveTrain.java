7700299fbfa56abbf2253fd2dfc0cbb7cbbeb391
Jake <jake.gads@gmail.com>
2021-03-02 18:40:37 -0500

    expofix

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -4,15 +4,17 @@ public abstract class DriveTrain {
 
 	public double expo(double value, double exponent) {
 		
-		double temp;
+		return Math.pow(value, exponent);
+
+		// double temp;
 		
-		temp = value;
-		for (int i = 1; i < exponent; i++)
-			temp *= temp;
-		if (value < 0.0 && temp > 0.0)  // preserve sign when multiplying by itself even number of times
-			return temp * -1.0;
-		else
-			return temp;
+		// temp = value;
+		// for (int i = 1; i < exponent; i++)
+		// 	temp *= temp;
+		// if (value < 0.0 && temp > 0.0)  // preserve sign when multiplying by itself even number of times
+		// 	return temp * -1.0;
+		// else
+		// 	return temp;
 	}
 
 	public abstract void calibrate(Inputs inputs);
